f337900032fc357672dfa5dba09b1b4cd90c1018
priya-270 <130386525+priya-270@users.noreply.github.com>
2023-05-11 06:49:04 +0530

    Update tomcatdeploy.groovy

--- vars/tomcatdeploy.groovy
+++ vars/tomcatdeploy.groovy
@@ -1,7 +1,7 @@
-def call(){
+def call(user,ip){
   sshagent(['tomcat-dev']) {
-     sh "scp target/hr-api.war ec2-user@172.31.32.245:/opt/jenkin-test/webapps/"
-      sh "ssh ec2-user@172.31.32.245 /opt/jenkins-test/bin/shutdown.sh"
-      sh "ssh ec2-user@172.31.32.245 /opt/jenkins-test/bin/startup.sh"
+     sh "scp target/hr-api.war ${user}@${ip}:/opt/jenkin-test/webapps/"
+    sh "ssh ${user}@${ip} /opt/jenkins-test/bin/shutdown.sh"
+      sh "ssh ${user}@${ip} /opt/jenkins-test/bin/startup.sh"
         }
    }
